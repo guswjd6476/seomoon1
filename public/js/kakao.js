556a77deaf9849119f704bf7d2d490f62663e441
Wonginoh <sspp4555@naver.com>
2022-07-05 15:44:56 +0900

    0705

--- public/js/kakao.js
+++ public/js/kakao.js
@@ -1,10 +1,16 @@
+
+
+
+document.getElementById('logout').style.display='none';
+
+
 Kakao.init('914acb3c822fd52dca46391c5ec5623a');
 Kakao.isInitialized();
 function kakaoLogin(){
     Kakao.Auth.login({
         success:function(response){
             Kakao.Api.request({
-                url:'/vs/user/me',
+                url:'/v2/user/me',
                 success:function(response){
                     console.log(response);
                     document.getElementById('user').innerText = 
@@ -17,5 +23,21 @@ function kakaoLogin(){
     })
 }
 function kakaoLogout(){
-    
+    if (!Kakao.Auth.getAccessToken()) {
+        Kakao.Auth.login({
+            success:function(response){
+                Kakao.Api.request({
+                    url:'/v1/user/unlink',
+                    success:function(response){
+                        console.log(response);
+                        document.getElementById('user').style.display='none';
+                        document.getElementById('login').style.display='block';
+                        document.getElementById('logout').style.display='none';
+                        alert(response.kakao_account.profile.nickname +'님 로그아웃 되었습니다.')
+                    }
+                })
+        Kakao.Auth.setAccessToken(undefinded)
+      }
+}
+)}
 }
